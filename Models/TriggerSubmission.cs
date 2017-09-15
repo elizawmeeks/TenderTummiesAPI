187f2c7f2f0227f1c80379df28d45d0e9ec55839
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-09-15 13:54:04 -0500

    Progress on trigger controller, needs feedback

---
+++ Models/TriggerSubmission.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+ 
+namespace TenderTummiesAPI.Models
+{
+    // Trigger symptom join table to keep track of the symptoms from each trigger.
+    // Includes trigger and symptoms, and whether it's a chronic or acute symptom.
+    public class TriggerSubmission
+    {
+        [Required]
+        public int FoodID { get; set; }
+
+        [Required]
+        public int ChildID { get; set; }
+
+        [Required]
+        public string Severity { get; set; }
+        
+        [Required]
+        public int SymptomID { get; set; }
+        
+        [Required]
+        public bool Acute { get; set; }
+
+        [Required]
+        public bool Chronic { get; set; }
+
+    }
+}
